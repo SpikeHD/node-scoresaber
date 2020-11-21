8d98a0052c1790497eecb1b22ab9a3a8b42cac48
spikegd <spikegdofficial@gmail.com>
2020-11-20 22:23:10 -0800

    Remove dist files

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = {
-  getPlayer: require('./Functions/getPlayer'),
-  getRequest: require('./Functions/getRequest'),
-  searchPlayers: require('./Functions/searchPlayers')
-};
