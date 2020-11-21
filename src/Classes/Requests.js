d1efced43129a4f18ab9c11dbd4bbe046b9dcbe4
spikegd <spikegdofficial@gmail.com>
2020-11-20 23:16:06 -0800

    get() in requests list

--- src/Classes/Requests.js
+++ src/Classes/Requests.js
@@ -1,6 +1,13 @@
 const axios = require('axios')
 const Base = require('./Base')
 
+/**
+ * Function used in object to get full object.
+ */
+async function get() {
+  return await require('../Functions/getRequest')(this.request)
+}
+
 class Requests extends Base {
   /**
    * Get list of top ranked requests
@@ -8,6 +15,10 @@ class Requests extends Base {
   async getTop() {
     let res = await axios.get(this.default_url + '/api/ranking/requests/top')
     let data = res.data.requests
+
+    data.map((r, i) => {
+      data[i].get = get
+    })
 
     this.top = data
 
@@ -20,6 +31,10 @@ class Requests extends Base {
   async getAll() {
     let res = await axios.get(this.default_url + '/api/ranking/requests/belowTop')
     let data = res.data.requests
+
+    data.map((r, i) => {
+      data[i].get = get
+    })
 
     this.all = data
 
