5d9fb5100d4047ee84e62d4706c385ae98e3524b
ShibeGuy <blakehharris@gmail.com>
2021-02-11 10:43:40 -0800

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  getPlayer: require('./Functions/getPlayer'),
-  getRequest: require('./Functions/getRequest'),
-  getAllRequests: require('./Functions/getAllRequests'),
-  getTopRequests: require('./Functions/getTopRequests'),
-  searchPlayers: require('./Functions/searchPlayers')
-}
